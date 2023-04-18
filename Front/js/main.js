4f7a6068cf8c16983aada2ddabb0e96ed675bbcb
Lekematti <leo.koskimaki@gmail.com>
2023-04-18 11:21:01 +0300

    Login screen done (modal)

---
+++ Front/js/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+//Modal
+const modal = document.getElementById("myModal");
+
+const btn = document.getElementById("login");
+
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+//Modal end
